c149ce283aabc183ae5c191d23617694e638bee0
viki064 <modiyam.vikram@gmail.com>
2023-09-14 16:58:26 -0400

    six

--- frontend/src/components/MainDashboard/editResume.js
+++ frontend/src/components/MainDashboard/editResume.js
@@ -1,13 +1,73 @@
 import { React, useState } from "react";
-import { Container, Row, Form, Col, Button } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Form,
+  Col,
+  Button,
+  Modal,
+  ModalFooter,
+} from "react-bootstrap";
 import DeleteIcon from "@mui/icons-material/Delete";
 import BorderColorIcon from "@mui/icons-material/BorderColor";
 // import FormExample from "../A_FormExample";
 
+function formatDateString(inputDateString) {
+  if (inputDateString === "Present") {
+    // Get the current date
+    const currentDate = new Date();
+
+    // Format the current date to "YYYY-MM"
+    const formattedDate = `${currentDate.getFullYear()}-${(
+      currentDate.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}`;
+
+    return formattedDate;
+  } else {
+    // If the input string is not "present," perform the original date formatting logic
+    const dateParts = inputDateString.split(" ");
+    const month = dateParts[0]; // Month name
+    const year = dateParts[1]; // Year
+    const dateObject = new Date(`${month} 1, ${year}`);
+    const formattedDate = `${dateObject.getFullYear()}-${(
+      dateObject.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}`;
+    return formattedDate;
+  }
+}
+
+function formatDateForInput(inputDateString) {
+  const dateParts = inputDateString.split("-");
+  const year = dateParts[0]; // Year
+  const month = parseInt(dateParts[1], 10); // Month as a number
+
+  // Get the month name based on the month number
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const formattedMonth = monthNames[month - 1]; // Subtract 1 to get the correct index
+
+  return `${formattedMonth} ${year}`;
+}
+
 function EditResume(props) {
   const [resume, setResume] = useState(props.resume ? { ...props.resume } : {});
   const [show, setShow] = useState(false);
-  const dark = props.dark;
 
   const [updatedValues, setUpdatedValues] = useState(
     resume ? { ...resume } : {}
@@ -78,12 +138,16 @@ function EditResume(props) {
     Technologies: "",
   });
 
+  const [workItemIndex, setWorkItemIndex] = useState(0);
   const [work, setWork] = useState([...updatedValues.WorkExperience]);
 
-  const workSubmit = (e) => {
-    e.preventDefault();
-    // Add the new sentence to the list of sentences
-    setWork([...work, workTemplate]);
+  const updateWork = () => {
+    const updatedWork = [...work];
+    if (workItemIndex >= 0 && workItemIndex < updatedWork.length) {
+      updatedWork[workItemIndex] = workTemplate;
+      setWork(updatedWork);
+      setShow(false);
+    }
   };
 
   const workDispExp = (item) => {
@@ -131,7 +195,7 @@ function EditResume(props) {
               className="mt-3"
               as="textarea"
               value={item.Responsibilities}
-              readOnly="false"
+              readOnly
               rows={5}
             />
             {/* <small>
@@ -177,8 +241,15 @@ function EditResume(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Call the updateFields function to update all fields
-    const updatedResume = { ...updatedValues, ProfessionalSummary: summary };
+    const updatedResume = {
+      ...updatedValues,
+      ProfessionalSummary: summary,
+      TechnicalSkills: skills,
+      Certifications: certs,
+      Achievements: achivement,
+      WorkExperience: work,
+    };
+
     updateFields(updatedResume);
     // console.log(updatedResume);
     // console.log(resume);
@@ -324,10 +395,35 @@ function EditResume(props) {
               </h4>
               {summary.map((itemSummary, index) => {
                 return (
-                  <span key={index}>
-                    {index + 1 + " - " + itemSummary}
-                    <br />
-                  </span>
+                  <Row>
+                    <Col>
+                      <span key={index}>
+                        {index + 1 + " - " + itemSummary}
+                        <br />
+                      </span>
+                    </Col>
+                    <Col style={{ maxWidth: "8vh" }}>
+                      <button
+                        type="button"
+                        style={{
+                          border: "none",
+                          backgroundColor: "transparent",
+                        }}
+                        onClick={() =>
+                          setSummary([
+                            ...summary.slice(0, index),
+                            ...summary.slice(index + 1),
+                          ])
+                        }
+                      >
+                        <DeleteIcon
+                          style={
+                            props.dark ? { color: "white" } : { color: "black" }
+                          }
+                        />
+                      </button>
+                    </Col>
+                  </Row>
                 );
               })}
             </Row>
@@ -436,10 +532,35 @@ function EditResume(props) {
               <h4 className="mt-4 text-decoration-underline">Certifications</h4>
               {certs.map((itemCerts, index) => {
                 return (
-                  <span key={index}>
-                    {index + 1 + " - " + itemCerts}
-                    <br />
-                  </span>
+                  <Row>
+                    <Col>
+                      <span key={index}>
+                        {index + 1 + " - " + itemCerts}
+                        <br />
+                      </span>
+                    </Col>
+                    <Col style={{ maxWidth: "8vh" }}>
+                      <button
+                        type="button"
+                        style={{
+                          border: "none",
+                          backgroundColor: "transparent",
+                        }}
+                        onClick={() =>
+                          setCerts([
+                            ...certs.slice(0, index),
+                            ...certs.slice(index + 1),
+                          ])
+                        }
+                      >
+                        <DeleteIcon
+                          style={
+                            props.dark ? { color: "white" } : { color: "black" }
+                          }
+                        />
+                      </button>
+                    </Col>
+                  </Row>
                 );
               })}
             </Row>
@@ -462,10 +583,35 @@ function EditResume(props) {
               <h4 className="mt-4 text-decoration-underline">Achievements</h4>
               {achivement.map((itemAchivement, index) => {
                 return (
-                  <span key={index}>
-                    {index + 1 + " - " + itemAchivement}
-                    <br />
-                  </span>
+                  <Row>
+                    <Col>
+                      <span key={index}>
+                        {index + 1 + " - " + itemAchivement}
+                        <br />
+                      </span>
+                    </Col>
+                    <Col style={{ maxWidth: "8vh" }}>
+                      <button
+                        type="button"
+                        style={{
+                          border: "none",
+                          backgroundColor: "transparent",
+                        }}
+                        onClick={() =>
+                          setAchivement([
+                            ...achivement.slice(0, index),
+                            ...achivement.slice(index + 1),
+                          ])
+                        }
+                      >
+                        <DeleteIcon
+                          style={
+                            props.dark ? { color: "white" } : { color: "black" }
+                          }
+                        />
+                      </button>
+                    </Col>
+                  </Row>
                 );
               })}
             </Row>
@@ -493,34 +639,190 @@ function EditResume(props) {
                 </Button>
               </h4>
             </Row>
-            {work.map((workitem, index) => {
-              return (
-                <div className="p-2 mb-2 border">
-                  <h4 className="text-decoration-underline">
-                    {"Experience: " + (index + 1)}
-                  </h4>
+            <div>
+              {work.map((workitem, index) => {
+                return (
+                  <div key={index} className="p-2 mb-2 border">
+                    <h4 className="text-decoration-underline">
+                      {"Experience: " + (index + 1)}
+                    </h4>
 
-                  {workDispExp(workitem)}
-                  <div className="mb-3 ms-2">
-                    <Button type="button" className="me-2" onClick={() => {}}>
-                      Edit <BorderColorIcon />
-                    </Button>
-                    <Button
-                      type="button"
-                      className="ms-2 bg-danger border border-danger"
-                      onClick={() =>
-                        setSkills([
-                          ...work.slice(0, index),
-                          ...work.slice(index + 1),
-                        ])
-                      }
-                    >
-                      Delete <DeleteIcon />
-                    </Button>
+                    {workDispExp(workitem)}
+                    <div className="mb-3 ms-2">
+                      <Button
+                        type="button"
+                        className="me-2"
+                        onClick={() => {
+                          setShow(true);
+                          setWorkTemplate(workitem);
+                          setWorkItemIndex(index);
+                        }}
+                      >
+                        Edit <BorderColorIcon />
+                      </Button>
+                      <Button
+                        type="button"
+                        className="ms-2 bg-danger border border-danger"
+                        onClick={() =>
+                          setWork([
+                            ...work.slice(0, index),
+                            ...work.slice(index + 1),
+                          ])
+                        }
+                      >
+                        Delete <DeleteIcon />
+                      </Button>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+              <Modal
+                show={show}
+                onHide={() => setShow(false)}
+                backdrop="static"
+                keyboard={false}
+                size="lg"
+                aria-labelledby="example-custom-modal-styling-title"
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="example-custom-modal-styling-title">
+                    {/* <h1>{companyName}</h1> */}
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form.Group as={Col} className="mt-3">
+                    <Row>
+                      <Form.Label column sm="3">
+                        Company Name
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Company Name"
+                        value={workTemplate.CompanyName}
+                        onChange={(e) => {
+                          setWorkTemplate({
+                            ...workTemplate,
+                            CompanyName: e.target.value,
+                          });
+                        }}
+                        style={{ maxWidth: "60vh" }}
+                      />
+                    </Row>
+                    <Row className="mt-3">
+                      <Form.Label column sm="3">
+                        Position
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Position"
+                        value={workTemplate.Position}
+                        onChange={(e) => {
+                          setWorkTemplate({
+                            ...workTemplate,
+                            Position: e.target.value,
+                          });
+                        }}
+                        style={{ maxWidth: "60vh" }}
+                      />
+                    </Row>
+                    <Row className="mt-3">
+                      <Form.Label column sm="3">
+                        Start Date
+                      </Form.Label>
+                      <Form.Control
+                        type="month"
+                        value={formatDateString(workTemplate.StartDate)}
+                        onChange={(e) => {
+                          setWorkTemplate({
+                            ...workTemplate,
+                            StartDate: formatDateForInput(e.target.value),
+                          });
+                        }}
+                        style={{ maxWidth: "60vh" }}
+                      />
+                    </Row>
+                    <Row className="mt-3">
+                      <Form.Label column sm="3">
+                        End Date
+                      </Form.Label>
+                      <Form.Control
+                        type="month"
+                        value={formatDateString(workTemplate.EndDate)}
+                        onChange={(e) => {
+                          setWorkTemplate({
+                            ...workTemplate,
+                            EndDate: formatDateForInput(e.target.value),
+                          });
+                        }}
+                        style={{ maxWidth: "60vh" }}
+                      />
+                    </Row>
+                    <Row className="mt-3">
+                      <Col style={{ maxWidth: "20vh" }}>
+                        <Form.Label column sm="3">
+                          Responsibilities
+                        </Form.Label>
+                      </Col>
+                      <Col>
+                        <Form.Control
+                          as="textarea"
+                          value={workTemplate.Responsibilities}
+                          onChange={(e) => {
+                            setWorkTemplate({
+                              ...workTemplate,
+                              Responsibilities: e.target.value,
+                            });
+                          }}
+                          placeholder="Write all the Responsibilities with seperater as '|'."
+                          rows={5}
+                        />
+                        <small>
+                          <strong>Note: </strong>Write all the responsibilities
+                          with seperater as '|'.
+                        </small>
+                      </Col>
+                    </Row>
+                    <Row className="mt-3">
+                      <Col style={{ maxWidth: "20vh" }}>
+                        <Form.Label className="mt-4">
+                          Skills/Technologies
+                        </Form.Label>
+                      </Col>
+                      <Col>
+                        <Form.Control
+                          className="mt-3"
+                          as="textarea"
+                          value={workTemplate.Technologies}
+                          onChange={(e) => {
+                            setWorkTemplate({
+                              ...workTemplate,
+                              Technologies: e.target.value,
+                            });
+                          }}
+                          rows={2}
+                          placeholder="Write all the Skills with seperater as '|'."
+                        />
+                        <small>
+                          <strong>Note: </strong>Write all the Skills with
+                          seperater as '|'.
+                        </small>
+                      </Col>
+                    </Row>
+                  </Form.Group>
+                </Modal.Body>
+                <ModalFooter>
+                  <Button
+                    type="button"
+                    className="me-2"
+                    onClick={() => {
+                      updateWork();
+                    }}
+                  >
+                    <BorderColorIcon /> Update
+                  </Button>
+                </ModalFooter>
+              </Modal>
+            </div>
 
             {/* Update resume Button */}
             <Form.Group className="m-4" as={Row}>
